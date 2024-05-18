17e684ae6c4443a0e8c7c4909397086271add399
csturm83 <csturm83@users.noreply.github.com>
2024-05-17 20:57:40 -0500

    Baseline QRCoder.Renderers project with PngByteRenderer prototype.

---
+++ QRCoder.Renderers/PngByteRendererSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace QRCoder.Renderers;
+
+public class PngByteRendererSettings : RenderSettings
+{
+    public int PixelsPerModule { get; set; }
+    public byte[] DarkColorRgba { get; set; }
+    public byte[] LightColorRgba { get; set; }
+    public bool DrawQuietZones { get; set; }
+}
