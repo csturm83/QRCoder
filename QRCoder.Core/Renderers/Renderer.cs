523aa85910fc3fb842d1f6d061dbc03f1fb36d94
csturm83 <csturm83@users.noreply.github.com>
2024-05-17 19:56:50 -0500

    Add generic static convenience method. Update sample renderer.

--- QRCoder.Core/Renderers/Renderer.cs
+++ QRCoder.Core/Renderers/Renderer.cs
@@ -1,7 +1,15 @@
-﻿namespace QRCoder.Renderers;
+﻿using QRCoder.Payloads;
+
+namespace QRCoder.Renderers;
 
 public abstract class Renderer<TRenderSettings, T> : IRenderer where TRenderSettings:RenderSettings
     {
+        // TODO: static convenience method for each QRCode.Create overload
+        public static TRenderer Create<TRenderer>(Payload payload, QRCodeSettings settings) where TRenderer : Renderer<TRenderSettings, T>, IRenderer, new()
+        {
+            return QRCode.Create(payload, settings)
+                .RenderAs<TRenderer>();
+        }
         protected QRCodeData QrCodeData { get; set; }
 
         protected Renderer() {
--- QRCoder.Core/Renderers/TestRenderer.cs
+++ QRCoder.Core/Renderers/TestRenderer.cs
@@ -1,7 +1,8 @@
 using QRCoder;
+using QRCoder.Payloads;
 using QRCoder.Renderers;
 
-public class TestRenderer : Renderer<TestRenderSettings, int>
+internal class TestRenderer : Renderer<TestRenderSettings, int>
 {
     public TestRenderer()
     {
@@ -14,6 +15,31 @@ public class TestRenderer : Renderer<TestRenderSettings, int>
     public override int Render(TestRenderSettings settings)
     {
         throw new NotImplementedException();
+    }
+
+    // TODO: other variations of overloads that call into the static Create<> method
+    public static int Render(Payload payload, QRCodeSettings qrSettings, TestRenderSettings settings)
+    {
+        return Create<TestRenderer>(payload, qrSettings).Render(settings);
+    }
+
+    private static void SampleTest()
+    {
+        Payload payload = null!;
+        QRCodeSettings qrSettings = null!;
+        TestRenderSettings renderSettings = null!;
+
+        // three-step rendering
+        int rendered3 = QRCode.Create(payload, qrSettings)
+            .RenderAs<TestRenderer>()
+            .Render(renderSettings);
+
+        // two-step rendering
+        int rendered2 = TestRenderer.Create<TestRenderer>(payload, qrSettings)
+            .Render(renderSettings);
+
+        // one-shot rendering
+        int rendered1 = TestRenderer.Render(payload, qrSettings, renderSettings);
     }
 }
 
